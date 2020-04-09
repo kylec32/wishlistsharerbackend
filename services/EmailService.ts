d3c7e0e2d6bdee54f9be14cec93aaea8205a1954
Kyle Carter <kylec32@gmail.com>
2020-04-08 22:28:48 -0600

    New Email API

--- services/EmailService.ts
+++ services/EmailService.ts
@@ -3,31 +3,57 @@ import * as request from 'request';
 export class EmailService {
 
     async sendTextMessage(to: string, subject: string, body: string): Promise<any> {
-        this.sendMessage({
-            from: 'no-reply@mail.wishlistsharer.tk',
-            to: to,
-            subject: subject,
-            text: body
-        });
+
+        this.sendMessage(
+        {
+            "Messages":[
+              {
+                "From": {
+                  "Email": "noreply@wishlistsharer.tk",
+                  "Name": "WishlistSharer"
+                },
+                "To": [
+                  {
+                    "Email": to
+                  }
+                ],
+                "Subject": subject,
+                
+                "TextPart": body
+              }
+            ]
+          });
     }
 
     async sendHtmlMessage(to: string, subject: string, body: string): Promise<any> {
-        this.sendMessage({
-            from: 'no-reply@mail.wishlistsharer.tk',
-            to: to,
-            subject: subject,
-            html: body
-        });
+        this.sendMessage(
+            {
+                "Messages":[
+                  {
+                    "From": {
+                      "Email": "noreply@wishlistsharer.tk",
+                      "Name": "WishlistSharer"
+                    },
+                    "To": [
+                      {
+                        "Email": to
+                      }
+                    ],
+                    "Subject": subject,
+                    "HTMLPart": body
+                  }
+                ]
+              });
     }
 
-    private async sendMessage(form: any): Promise<any> {
+    private async sendMessage(body: any): Promise<any> {
         return new Promise((resolve, reject) => {
-            request.post('https://api.mailgun.net/v3/mail.wishlistsharer.tk/messages', {
+            request.post('https://api.mailjet.com/v3.1/send', {
                 'auth': {
-                    'user': 'api',
+                    'user': '30a23f52f366cf26048627a52171dbc4',
                     'password': process.env.MAILGUN_API_KEY
                 },
-                'form': form
+                'body': JSON.stringify(body)
             }, (error: any, response: request.Response, body: any) => {
                 if(error) {
                     reject(error);
